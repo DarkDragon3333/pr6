a8c7a3748d60cfd043d6ce0b8d553edd0d446497
DarkDragon3333 <minaichiev-dragon2017@yandex.ru>
2022-09-19 13:07:35 +0300

    Not ready

--- src/ru/mirea/pr6/Main.java
+++ src/ru/mirea/pr6/Main.java
@@ -1,37 +1,9 @@
 package ru.mirea.pr6;
 
 public class Main {
-    int x = 0;
-    public void Sort(){
-        int[] arr = {1, 2, 1, 0 ,5};
-
-        for (int i = 0; i < 5; i++) {
-            if (i == 4){ System.out.println(arr[i]);}
-            else { System.out.print(arr[i] + ", ");}
-        }
-
-        System.out.print("");
-
-        for (int i = 1; i < 5; i++){
-            if (arr[i - 1] > arr[i]){
-                x = arr[i - 1];
-                arr[i - 1] = arr[i];
-                arr[i] = x;
-            }
-        }
-
-        for (int i = 0; i < 5; i++) {
-            if (i == 4){ System.out.println(arr[i]);}
-            else { System.out.print(arr[i] + ", ");}
-        }
-        System.out.printf("45");
-        System.out.println("123");
-
-        System.out.printf("45");
-        System.out.println("123");
-    }
 
     public static void main(String[] args) {
-
+        Student st = new Student();
+        st.Sort();
     }
 }
--- src/ru/mirea/pr6/Student.java
+++ src/ru/mirea/pr6/Student.java
@@ -1,5 +1,46 @@
 package ru.mirea.pr6;
 
 public class Student {
+    int x = 0;
 
+    public void Sort() {
+        int[] arr = {1, 2, 1, 0, 5};
+
+        for (int i = 0; i < 5; i++) {
+            if (i == 4) {
+                System.out.println(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+
+        System.out.print("");
+
+        for (int left = 0; left < arr.length; left++) {
+            // Вытаскиваем значение элемента
+            int value = arr[left];
+            // Перемещаемся по элементам, которые перед вытащенным элементом
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                // Если вытащили значение меньшее — передвигаем больший элемент дальше
+                if (value < arr[i]) {
+                    arr[i + 1] = arr[i];
+                } else {
+                    // Если вытащенный элемент больше — останавливаемся
+                    break;
+                }
+            }
+            // В освободившееся место вставляем вытащенное значение
+            arr[i + 1] = value;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (i == 4) {
+                System.out.println(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+
+    }
 }
